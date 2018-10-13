fe8d74f2798a3ff5deee2c10726c6b63bafc5f13
InklingSplasher <klotzjan@mailbox.org>
2018-10-13 20:00:44 +0200

    Fixed many stuff, deleted unused code, added color dependencie

--- index.js
+++ index.js
@@ -6,34 +6,31 @@ const fs = require("fs")
 
 // What happens when the bot is started
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("with Javascript code!");
+  console.log(`Logged in as ${bot.user.username}...`);
+  bot.user.setGame("with JavaScript code!");
 });
 
-// First commands
+// Command rules
 bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+  if(message.author.bot) return; // Checks if command author is the bot iself.
+  if(message.channel.type === "dm") return; // Checks if the command is used in DMs.
 
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+// Variables
+var author = message.author; // The person who sent the message.
+var msg = message.content.toUpperCase(); // Takes a message and makes it all upper-case.
+var cont = message.content // Raw message content
+var prefix = botconfig.prefix; // The prefix of the bot (stands before every command).
+var guild = msg.guild;
+var channel = msg.channel;
 
-    // Variables
-    var sender = message.author; // The person who sent the message.
-    var msg = message.content.toUpperCase(); // Takes a message and makes it all upper-case.
-    var prefix = botconfig.prefix; // The prefix of the bot (stands before every command).
-
-
-// Ping / Pong Commands
-  if(cmd === `${prefix}hello`){
-    return message.channel.send("Hello!");
-  }
+// Say command
+  var invoke = cont.split(" ")[0].substr(botconfig.prefix.length);
+  var args = cont.split(" ").slice(1);
+  console.log(invoke, args)
 
   if(msg === prefix + 'PING'){
     message.channel.send("Pong!") // Sends a message to the channel where the command had been used with the content "Pong!".
   }
 });
-
 
 bot.login(botconfig.token);
