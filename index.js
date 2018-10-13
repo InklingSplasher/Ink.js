8aef02bcf82a69a9d72c9dd53d37d101f09f59c0
InklingSplasher <klotzjan@mailbox.org>
2018-10-13 19:03:51 +0200

    Added the ping command and added some more descriptions

--- index.js
+++ index.js
@@ -14,15 +14,25 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-// !hello command
+    // Variables
+    var sender = message.author; // The person who sent the message.
+    var msg = message.content.toUpperCase(); // Takes a message and makes it all upper-case.
+    var prefix = botconfig.prefix; // The prefix of the bot (stands before every command).
+
+
+// Ping / Pong Commands
   if(cmd === `${prefix}hello`){
     return message.channel.send("Hello!");
   }
+
+  if(msg === prefix + 'PING'){
+    message.channel.send("Pong!") // Sends a message to the channel where the command had been used with the content "Pong!".
+  }
 });
+
 
 bot.login(botconfig.token);
